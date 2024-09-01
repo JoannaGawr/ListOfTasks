90aae5975a1432a5f966de385a3b937af09a9002
JG <gawryszewska.joanna@gmail.com>
2024-09-01 18:29:21 +0200

    Initial Commit

---
+++ ListOfTasks/ListOfTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListOfTasksApp.swift
+//  ListOfTasks
+//
+//  Created by Joanna Gawryszewska on 2024-09-01.
+//
+
+import SwiftUI
+
+@main
+struct ListOfTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListOfTasksUITests/ListOfTasksUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ListOfTasksUITestsLaunchTests.swift
+//  ListOfTasksUITests
+//
+//  Created by Joanna Gawryszewska on 2024-09-01.
+//
+
+import XCTest
+
+final class ListOfTasksUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
